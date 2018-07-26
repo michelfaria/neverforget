bfe6df7ae40c3dd741de9169079d19521a6e9240
Michel <michelfariarj@gmail.com>
2018-07-26 16:48:19 -0300

    Initial commit

---
+++ src/never/NeverForget.java
@@ -0,0 +1,9 @@
+package never;
+
+import javax.swing.JOptionPane;
+
+public class NeverForget {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Works!", "Hello!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
